b1c268faad09313f43840b89ccaade5e2fe4900e
Jose Seie <joseeugenioseie@hotmail.com>
2017-10-16 16:24:34 +0200

    adicao de atributos

---
+++ src/explicacao/poo/aula3/Estudante.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package explicacao.poo.aula3;
+
+/**
+ *
+ * @author José Seie
+ */
+public class Estudante {
+    //modificadores de acesso;
+    //public
+    //private
+    //protected
+    
+    
+   private String nomecompleto;
+   private String nome;
+   private String apelido;
+   private int idade;
+         
+   
+    
+}
